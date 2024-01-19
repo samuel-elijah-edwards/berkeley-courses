c61c7a11af22570b7afba56ac0a1f5edad784cb5
Samuel Edwards <s_edwards@berkeley.edu>
2024-01-19 00:10:32 -0800

    beginning work on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,9 +5,26 @@ import Layout from "../components/Layout";
 function Home() {
   return (
     <Layout>
-      <h1 className="flex justify-center text-3xl mx-2 mb-2">
+      <h1 className="pb-5  flex justify-center text-3xl mx-2 mb-2">
         Get insights on your courses
       </h1>
+      <section className="grid grid-cols-3 pt-12">
+        <div className="flex justify-center">
+          <div className="col-start-1 border-2 border-white w-80 h-96 flex justify-center rounded-lg animate-customScale shadow-2xl">
+            <div className="animate-none text-xl">Look at Course</div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="col-start-1 border-2 border-white w-80 h-96 flex justify-center rounded-lg animate-customScale shadow-2xl">
+            <div className="text-xl">Look at Course</div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="col-start-1 border-2 border-white w-80 h-96 flex justify-center rounded-lg animate-customScale shadow-2xl">
+            <div className="animate-none text-xl">Look at Course</div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      keyframes: {
+        customScale: {
+          // Corrected keyframes syntax
+          "0%, 100%": { transform: "scale(1.04)" },
+          "50%": { transform: "scale(1)" },
+        },
+      },
+      animation: {
+        customScale: "customScale 3s ease-in-out infinite", // Corrected animation syntax
+      },
       colors: {
         berkeleyBlue: "#003262",
         metallicGold: "#bc9b6a",
